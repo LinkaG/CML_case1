0fd39c528ee982d72e4aaa071d0e0fb61c5a2ec5
Lina Rusinova <zrusinova@ussc.ru>
2023-11-06 16:06:33 +0500

    first commit

---
+++ train.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read in data
+X_train = np.genfromtxt("data/train_features.csv")
+y_train = np.genfromtxt("data/train_labels.csv")
+X_test = np.genfromtxt("data/test_features.csv")
+y_test = np.genfromtxt("data/test_labels.csv")
+
+# Fit a model
+depth = 10
+clf = RandomForestClassifier(max_depth=depth)
+clf.fit(X_train, y_train)
+
+acc = clf.score(X_test, y_test)
+print(acc)
+
+metrics = """
+Accuracy: {:10.4f}
+
+![Confusion Matrix](plot.png)
+""".format(acc)
+with open("metrics.txt", "w") as outfile:
+    outfile.write(metrics)
+
+# Plot it
+predictions = clf.predict(X_test)
+cm = confusion_matrix(y_test, predictions, labels=clf.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+                              display_labels=clf.classes_)
+plt.savefig("plot.png")
